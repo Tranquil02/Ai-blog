49ec02abf2cc9682d7ec218c0b153d94c3ddc697
Tranquil023 <prashantkashyap2707@gmail.com>
2026-02-14 03:07:08 +0530

    Remove third-party script from root layout to restore rendering

--- app/layout.jsx
+++ app/layout.jsx
@@ -48,7 +48,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="antialiased">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
